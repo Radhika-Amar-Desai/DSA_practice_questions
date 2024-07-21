cea5677d1583add5bca687bcfdd5443d5f7a13a7
Radhika Desai <radhikaamar.desai2022@vitstudent.ac.in>
2024-07-21 23:23:49 +0530

    Added recursive and backtracking solution for n_digit_increasing

--- backtracking/N_digit_increasing.py
+++ backtracking/N_digit_increasing.py
@@ -1,19 +1,32 @@
-digits = range(1,10)
+digits = range ( 1 , 10 )
 
-output = []
+n_digit_nums_increasing = []
 
-def n_digit_increasing ( n , nums = [] ):
+def add_digit_at_nth_place ( s , n , digit ):
 
+    chars = list ( s )
+    chars [ len(s) - n ] = str ( digit )
+
+    return "".join ( chars )
+
+def remove_digit_from_nth_place ( s , n ):
+    chars = list ( s )
+    chars [ len ( s ) - n  ] = '0'
+
+    return "".join ( chars )
+
+def n_digit_increasing ( n : int , s : str ):
     if n == 0:
-        if nums not in output:
-            output.append ( nums )
+        n_digit_nums_increasing.append ( s )
         return 
     
-    for num in digits:
-        if nums and nums[-1] < num:
-            n_digit_increasing ( n-1 , nums + [num] )
-        elif not nums:
-            n_digit_increasing ( n-1 , nums + [num] )
-            
-n_digit_increasing ( 3 ) 
-print ( output )
+    for digit in range ( 1, 10 ) :
+        
+        if int ( s [ 0 ] ) < digit:
+                s = add_digit_at_nth_place ( s , n, digit )
+                n_digit_increasing ( n - 1, s )
+                s = remove_digit_from_nth_place ( s , n )
+
+n_digit_increasing ( 3, '000' )
+
+print ( n_digit_nums_increasing )
